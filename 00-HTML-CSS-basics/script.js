c4bd1f10fe9ff01c889b6488532f248d030af3d4
Lucas <lucasrafael.romero@gmail.com>
2017-01-22 14:32:55 -0300

    punto 9 andando

--- 00-HTML-CSS-basics/script.js
+++ 00-HTML-CSS-basics/script.js
@@ -10,11 +10,40 @@ function joke(){
   response.then(function(res){
     text = JSON.parse(res);
     document.getElementById("joke").innerHTML = text.value.joke;
-    handleError(res);
   })
-  .catch( function handleError (res){
+  .catch( function(res){
     let element = document.getElementById("fade");
     element.style.color = "Red";
+  });
+}
+
+function repoSearch(){
+  let oldList = document.getElementById("repoList");
+  if (oldList) {
+    document.body.removeChild(oldList);
+  }
+  let response = ajaxCall({method:"GET", url:"https://api.github.com/search/repositories?q='JavaScript"});
+  response.then(function(res){
+    //console.log(res);
+    
+    let repoResponse = JSON.parse(res);
+
+    let section = document.createElement("section");
+    section.style.float = "right";
+    section.id = "repoList";
+    document.body.appendChild(section);
+    
+    let list = document.createElement("ul");
+    list.style.listStylePosition = "inside";
+    list.style.float = "right";
+    section.appendChild(list);
+
+    for (let i = 0; i <= repoResponse.items.length - 1; i++) {
+      let fullName = repoResponse.items[i].full_name;
+      let li = document.createElement("li");
+      li.innerHTML = fullName
+      list.appendChild(li);
+    }
   });
 }
 
@@ -26,7 +55,18 @@ function ajaxCall(conf){
 
     let xhttp = new XMLHttpRequest();
     xhttp.open(conf.method, conf.url, conf.async);
-    xhttp.send();
+    switch(conf.method){
+      case "POST" :
+        xhttp.send(conf.params);
+        break;
+      
+      case "GET" :
+      default :
+        xhttp.send();
+        break;
+      
+    }
+    
     xhttp.onload = function() {
       if (this.status >= 200 && this.status < 300) {
         resolve(xhttp.response);
