4566a069d79316812ad99d7e732a851c3d6caffc
Lucas <lucasrafael.romero@gmail.com>
2017-02-15 10:39:10 -0300

    topic 4 punto 6

--- 04-HTML5-APIs/script.js
+++ 04-HTML5-APIs/script.js
@@ -1,3 +1,12 @@
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
 //Abrir la base de datos
 let db;
 let request = window.indexedDB.open("DatabaseTest");
@@ -32,7 +41,7 @@ ws.onmessage = function (e){
   console.log("echo: " + e.data);
 };
 
-//Dibujar en el canvas
+//funciones para el primer canvas
 
 function randomColor() { // http://www.paulirish.com/2009/random-hex-color-code-snippets/
   return '#'+Math.floor(Math.random()*16777215).toString(16);
@@ -48,7 +57,7 @@ function fillBackgroundColor(canvas, context) {
 
 
 // Function to draw Square on canvas
-function drawSquare(canvas, context, fill) {
+function drawRandomSquare(canvas, context, fill) {
   // Here we created a random width and height for a square
   let w = Math.floor(Math.random() * 40);
   let x = Math.floor(Math.random() * canvas.width);
@@ -73,6 +82,65 @@ function drawCircle(canvas, context, fill) {
   if(fill) {context.fill();} else {context.stroke();}
 }
 
+function drawStableSquare(rectangle, context) {
+        context.beginPath();
+        context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        context.fillStyle = '#8ED6FF';
+        context.fill();
+        context.lineWidth = rectangle.borderWidth;
+        context.strokeStyle = 'black';
+        context.stroke();
+}
+
+let rectangle = {
+        x: 0,
+        y: 75,
+        width: 100,
+        height: 50,
+        borderWidth: 5,
+        direction: "right"
+};
+
+function animate (rectangle, canvas, context, startTime, linearSpeed){
+    // update
+    let time = (new Date()).getTime() - startTime;
+
+    let newX;
+
+    if (rectangle.direction === "right") {
+      //Si va para la derecha, actualizo 
+      newX = linearSpeed * time / 1000;
+
+      if(newX < canvas.width - rectangle.width - rectangle.borderWidth / 2) {
+        rectangle.x = newX;
+      } else {
+        rectangle.direction = "left";
+        startTime = (new Date()).getTime();
+      }
+
+    } else {
+
+      newX = (canvas.width - rectangle.width - rectangle.borderWidth / 2) - linearSpeed * time / 1000;
+
+      if (newX > rectangle.borderWidth / 2) {
+        rectangle.x = newX;
+      } else {
+        rectangle.direction = "right";
+        startTime = (new Date()).getTime();
+      }
+
+    }
+
+    // clear and redraw
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawStableSquare(rectangle, context);
+
+    // request new frame
+    window.requestAnimFrame( function(){ animate(rectangle, canvas, context, startTime, 100) });
+  }
+
+
+
 
 //Modulo de angular
 
@@ -81,7 +149,6 @@ review.controller('reviewCtrl', function($scope, $localStorage){
 
   //Funciones para guardar y borrar el texto
   //TODO: meter los handlers de success y error que faltan.
-
   $scope.$storage = $localStorage;
 
   $scope.saveContents = function(){
@@ -118,7 +185,6 @@ review.controller('reviewCtrl', function($scope, $localStorage){
   }
 
   //Drag and drop de archivos
-
   let holder = document.getElementById("holder");
 
   holder.ondragover = function() {
@@ -148,11 +214,13 @@ review.controller('reviewCtrl', function($scope, $localStorage){
       return false;
   };
 
+  //Mandar el mensaje del websocket
   $scope.wsEcho = function(){
       if ($scope.text) {ws.send($scope.text);}
       else {ws.send("default message")};
-    };
+  };
 
+  //Dibujar en el primer canvas
   $scope.drawShapes = function() {
     let canvas = document.getElementById("canvas");
     let context = canvas.getContext("2d");
@@ -169,10 +237,21 @@ review.controller('reviewCtrl', function($scope, $localStorage){
       let fill = Math.random() < 0.5 ? true : false;
       context.fillStyle = context.strokeStyle = randomColor();
 
-      if (shape == "square") {drawSquare(canvas, context, fill);}
+      if (shape == "square") {drawRandomSquare(canvas, context, fill);}
 
       if (shape == "circle") {drawCircle(canvas, context, fill);}
     }
   };
+
+  //Dibujar cuando se carga la pagina  
+  angular.element(document).ready(function () {$scope.drawShapes();});
+
+
+  //Animar cuando se carga la pantalla
+  let canvas2 = document.getElementById("canvas2");
+  let context2 = canvas2.getContext("2d");
+  angular.element(document).ready(function () {
+    animate(rectangle, canvas2, context2, (new Date()).getTime(), 100);
+  });
 
 });
